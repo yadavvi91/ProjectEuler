680c7104e80ed91cdd6a73b01a0b1f9397b3f5d4
Vishal Yadav <yadavvi91@gmail.com>
2014-10-20 00:18:35 +0530

    Added "Constant Time" O(1) implementation of the problem.

--- com/yadavvi/projecteuler/SumBelowN.java
+++ com/yadavvi/projecteuler/SumBelowN.java
@@ -27,7 +27,7 @@ public class SumBelowN {
 				// when the firstMultiple is lesser than secondMultiple
 				// add it to sum and increment it
 
-				// This is when <firstMultiple, secondMultiple> is 
+				// This is when <firstMultiple, secondMultiple> is
 				// <3, 5>, <6, 10>, <9, 10>, <12, 15>, <18, 20>
 				sum += firstMultiple;
 				firstMultiple += three;
@@ -36,7 +36,7 @@ public class SumBelowN {
 				// when the secondMultiple is lesser than firstMultiple
 				// add it to sum and increment it
 
-				// This is when <firstMultiple, secondMultiple> is 
+				// This is when <firstMultiple, secondMultiple> is
 				// <5, 6>, <10, 12>
 				sum += secondMultiple;
 				secondMultiple += five;
@@ -44,6 +44,30 @@ public class SumBelowN {
 		}
 
 		return sum;
+	}
+
+	/**
+	 * Returns the sum of all the multiples of {@code value} smaller than
+	 * {@code limit}.
+	 */
+	private double sumOfSequence(int value, int limit) {
+		int count = (limit - 1) / value;
+		return value * count * (count + 1) / 2;
+	}
+
+	private double getSumConstTime(int N) {
+		// The numbers that are multiple of 3 and less than N are of type
+		// <3, 6, 9, 12, 15, ....>.
+		// Thus they can be written as <3*1, 3*2, 3*3, 3*4, 3*5, ....> and
+		// all these numbers form a sequence 3*(1, 2, 3, 4, .....N/3).
+		// The sum of this sequence can be given by n*(n+1)/2.
+
+		// To find the sum of all the numbers divisible by 3 & 5, we take
+		// the sum of the sequence of 3, 5 and Subtract the sum of multiples of
+		// LCM of 3 and 5 i.e. 15.
+
+		// The final result should be something like -
+		return sumOfSequence(3, N) + sumOfSequence(5, N) - sumOfSequence(15, N);
 	}
 
 	public static void main(String[] args) {
@@ -56,6 +80,9 @@ public class SumBelowN {
 		SumBelowN sumBelowN = new SumBelowN();
 
 		System.out.println("Sum: " + sumBelowN.getSum(i));
+		System.out
+				.println("Constant time Sum: " + sumBelowN.getSumConstTime(i));
+
 		sc.close();
 	}
 }
